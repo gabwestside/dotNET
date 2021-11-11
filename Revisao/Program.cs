b06c49f5e803fc600c6996937e6d441d7c01b5a0
gabwestside <gabrielnfl13@gmail.com>
2021-11-11 16:10:41 -0300

    inital commit

---
+++ Revisao/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //ToDo: adicionar aluno
+                        break;
+                    case "2":
+                        //ToDo: listar aluno
+                        break;
+                    case "3":
+                        //ToDo: calcular media geral
+                        break;
+                    default:
+
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                Console.WriteLine("Informe a opção desejada: ");
+                Console.WriteLine("1 - Inserir novo aluno");
+                Console.WriteLine("2 - Listar alunos");
+                Console.WriteLine("3 - Calcular média geral");
+                Console.WriteLine();
+
+                opcaoUsuario = Console.ReadLine();
+            }
+        }
+    }
+}
