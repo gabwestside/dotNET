4a54522e80bcb20bc3ad5c8c171965bf7cfd6385
Gabriel <gabrielnfl13@gmail.com>
2021-11-24 16:14:52 -0300

    Atulizações

---
+++ LessonsCsharp/GetStartedFunctions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GetStartedFunctions {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Escreva três números: ");
+            //int value1 = int.Parse(Console.ReadLine());
+            //int value2 = int.Parse(Console.ReadLine());
+            //int value3 = int.Parse(Console.ReadLine());
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            //if (value1 > value2 && value1 > value3) {
+            //    Console.WriteLine("Maior = " + value1);
+            //}
+            //else if (value2 > value3) {
+            //    Console.WriteLine("Maior = " + value2);
+            //}
+            //else {
+            //    Console.WriteLine("Maior = " + value3);
+            //}
+
+            double resultado = Maior(a, b, c);
+            Console.WriteLine("Maior = " + resultado);
+        }
+
+        static int Maior(int a, int b, int c) {
+            int m;
+            if (a > b && a > c) {
+                m = a;
+            }
+            else if (b > c) {
+                m = b;
+            }
+            else {
+                m = c;
+            }
+            return m;
+        }
+    }
+}
