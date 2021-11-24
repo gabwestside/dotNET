4d9812557ab723dda95de7f4c616be296c60bf9c
Gabriel <gabrielnfl13@gmail.com>
2021-11-24 11:10:18 -0300

    Subindo atualizações do dia

---
+++ .NET/LessonsCsharp/Exercicios/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computer";
+            string produto2 = "Table office";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+
+            Console.WriteLine("Produtos: ");
+            Console.WriteLine($"{produto1}, cujo o preço é $ {preco1}", preco1.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
