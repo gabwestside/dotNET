eb02495f1fd4b3ee44251790201e4cd65b757038
Gabriel <gabrielnfl13@gmail.com>
2021-11-25 15:10:23 -0300

    Atualizações

---
+++ LessonsCsharp/ExercicioDeFixacao7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExercicioDeFixacao7 {
+    class Program {
+        static void Main(string[] args) {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e a altura do retangulo:");
+            retangulo.width = double.Parse(Console.ReadLine());
+            retangulo.height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area = " + retangulo.Area());
+        }
+    }
+}
---
+++ LessonsCsharp/ExercicioDeFixacao7/Retangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioDeFixacao7 {
+    class Retangulo {
+        public double width;
+        public double height;
+
+        public double Area() {
+            return height * width;
+        }
+
+        //public double Perimetro() {
+
+        //}
+
+        //public double Diagonal() {
+
+        //}
+    }
+}
