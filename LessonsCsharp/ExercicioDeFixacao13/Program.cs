d989fed1d513b6c92eaab63adb5700866aace89a
Gabriel <gabrielnfl13@gmail.com>
2021-11-29 15:08:55 -0300

    atualizações

---
+++ LessonsCsharp/ExercicioDeFixacao13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioDeFixacao13 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            RentRoom[] vect = new RentRoom[10];
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine($"Rent #{i+1}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                vect[room] = new RentRoom(name, email);
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Busy rooms: ");
+            for (int j = 0; j < 10; j++) {
+                if (vect[j] != null) {
+                    Console.WriteLine(j + ": " + vect[j]);
+                }
+            }
+
+        }
+    }
+}
---
+++ LessonsCsharp/ExercicioDeFixacao13/RentRoom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioDeFixacao13 {
+    class RentRoom {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public RentRoom(string name, string email) {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString() {
+            return Name + ", " + Email;
+        }
+    }
+}
