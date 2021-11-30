e27c5f4b285b5eda86397a3b014409cca1b64e3b
gabwestside <gabrielnfl13@gmail.com>
2021-11-30 16:59:18 -0300

    Atualizações

---
+++ LessonsCsharp/ExercicioDeFixacao14/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ExercicioDeFixacao14 {
+    class Employee {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, double salary) {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double per) {
+
+        }
+
+        public override string ToString() {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ LessonsCsharp/ExercicioDeFixacao14/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioDeFixacao14 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine($"Empolyee #{i+1}:");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Employee(id, name, salary));
+
+                Console.WriteLine();
+            }
+
+            //Console.Write("Enter the employee id that will have salary increase: ");
+            //double salaryIncrease = double.Parse(Console.ReadLine());
+            //Console.WriteLine();
+            //Console.Write("Enter the percentage: ");
+            //double per = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Update list of employees:");
+            foreach (Employee obj in list) {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
