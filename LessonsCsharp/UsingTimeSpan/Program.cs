42f6b0a8c501cf980f3d45f180397f9315d79499
gabwestside <gabrielnfl13@gmail.com>
2021-12-02 13:38:40 -0300

    Att

--- LessonsCsharp/UsingTimeSpan/Program.cs
+++ LessonsCsharp/UsingTimeSpan/Program.cs
@@ -3,7 +3,36 @@
 namespace UsingTimeSpan {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            TimeSpan t = new TimeSpan(2, 3, 5, 7, 11);
+
+            Console.WriteLine(t);
+
+            Console.WriteLine("Days: " + t.Days);
+            Console.WriteLine("Hours: " + t.Hours);
+            Console.WriteLine("Minutes: " + t.Minutes);
+            Console.WriteLine("Milliseconds: " + t.Milliseconds);
+            Console.WriteLine("Seconds: " + t.Seconds);
+            Console.WriteLine("Ticks: " + t.Ticks);
+            Console.WriteLine("TotalDays: " + t.TotalDays);
+            Console.WriteLine("TotalHours: " + t.TotalHours);
+            Console.WriteLine("TotalMinutes: " + t.TotalMinutes);
+            Console.WriteLine("TotalSeconds: " + t.TotalSeconds);
+            Console.WriteLine("TotalMilliseconds: " + t.TotalMilliseconds);
+
+            TimeSpan t1 = new TimeSpan(1, 30, 10);
+            TimeSpan t2 = new TimeSpan(0, 10, 5);
+
+            TimeSpan sum = t1.Add(t2);
+            TimeSpan dif = t1.Subtract(t2);
+            TimeSpan mult = t2.Multiply(2.0);
+            TimeSpan div = t2.Divide(2.0);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(sum);
+            Console.WriteLine(dif);
+            Console.WriteLine(mult);
+            Console.WriteLine(div);
         }
     }
 }
