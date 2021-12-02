ffaef58f1d1bc80ff3543d0045d530fcfbf53954
gabwestside <gabrielnfl13@gmail.com>
2021-12-02 10:39:52 -0300

    Atualizações

---
+++ LessonsCsharp/FormatingDate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FormatingDate {
+    class Program {
+        static void Main(string[] args) {
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58);
+            string s1 = d.ToLongDateString();
+            string s2 = d.ToLongTimeString();
+            string s3 = d.ToShortDateString();
+            string s4 = d.ToShortTimeString();
+            string s5 = d.ToString();
+            string s6 = d.ToString("yyyy-MM-dd HH:mm:ss");
+            string s7 = d.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
+            Console.WriteLine(s7);
+        }
+    }
+}
---
+++ LessonsCsharp/WorkingWithDate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WorkingWithDate {
+    class Program {
+        static void Main(string[] args) {
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58, 275);
+            Console.WriteLine(d);
+            Console.WriteLine("1) Date: " + d.Date);
+            Console.WriteLine("2) Day: " + d.Day);
+            Console.WriteLine("3) DayOfWeek: " + d.DayOfWeek);
+            Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
+            Console.WriteLine("5) Hour: " + d.Hour);
+            Console.WriteLine("6) Kind: " + d.Kind);
+            Console.WriteLine("7) Millisecond: " + d.Millisecond);
+            Console.WriteLine("8) Minute: " + d.Minute);
+            Console.WriteLine("9) Month: " + d.Month);
+            Console.WriteLine("10) Second: " + d.Second);
+            Console.WriteLine("11) Ticks: " + d.Ticks);
+            Console.WriteLine("12) TimeOfDay: " + d.TimeOfDay);
+            Console.WriteLine("13) Year: " + d.Year);
+        }
+    }
+}
