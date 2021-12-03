2045dd838c41ce7210f83d9f1737ec47f19e49fd
gabwestside <gabrielnfl13@gmail.com>
2021-12-03 15:50:46 -0300

    Atualizações com Enums, começo de projeto

---
+++ MainSolution/SolvedExercise/Entities/Worker.cs
@@ -0,0 +1,50 @@
+﻿using SolvedExerciseAboutEnum.Entities.Enums;
+using System.Collections.Generic;
+
+namespace SolvedExerciseAboutEnum.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Departament { get; set; }
+        public List<HourContract> Contracts { get; set; }
+
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int mouth)
+        {
+            double sum = BaseSalary;
+            foreach(HourContract contract in Contracts)
+            {
+                if ( contract.Date.Year == year && contract.Date.Month == mouth)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
+    }
+}
