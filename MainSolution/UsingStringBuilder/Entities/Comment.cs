2697b4c697af537e3f79e0d8194da97f3560c3c4
gabwestside <gabrielnfl13@gmail.com>
2021-12-06 13:04:07 -0300

    Att

---
+++ MainSolution/UsingStringBuilder/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingStringBuilder.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
