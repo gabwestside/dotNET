4e6b9b4afe668007002c1b0c2036c45bc5a964ed
gabwestside <gabrielnfl13@gmail.com>
2021-12-08 10:02:18 -0300

    Att 1.0.14 Upcasting & Downcasting

---
+++ MainSolution/UsingUpcastingAndDowncasting/Entities/BusinessAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingUpcastingAndDowncasting.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit)
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ MainSolution/UsingUpcastingAndDowncasting/Entities/SavingsAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingUpcastingAndDowncasting.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
