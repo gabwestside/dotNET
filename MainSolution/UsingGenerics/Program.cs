655edece0146c69100d21f7e746c4b418b84e02c
gabwestside <gabrielnfl13@gmail.com>
2021-12-14 15:49:07 -0300

    Att 1.0.23 Adicionando o projeto de xadrez

---
+++ MainSolution/UsingGenerics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UsingGenerics.Entities;
+
+namespace UsingGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ MainSolution/UsingIComparable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using UsingIComparable.Entities;
+
+namespace UsingIComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\gabriel.moura.FITBANK\Documents\Gabs\tst.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
