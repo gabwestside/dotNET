2cf8903c19cc1ca8cfc41a4bc43e74bba9016c7f
gabwestside <gabrielnfl13@gmail.com>
2021-12-15 14:51:38 -0300

    Att 1.0.25

--- MainSolution/UsingGenerics/Program.cs
+++ MainSolution/UsingGenerics/Program.cs
@@ -7,7 +7,23 @@ namespace UsingGenerics
     {
         static void Main(string[] args)
         {
+            PrintService<int> printService = new PrintService<int>();
+            
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
 
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            int a = printService.First();
+            int b = a + 2;
+            Console.WriteLine(b);
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
         }
     }
 }
