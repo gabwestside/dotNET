ac00b97807c056427298453868cd61dad4feb5c6
gabwestside <gabrielnfl13@gmail.com>
2021-12-15 15:22:38 -0300

    Att 1.0.26

---
+++ MainSolution/UsingGenericRestrictions/Entities/CalculationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UsingGenericRestrictions.Entities
+{
+    class CalculationService
+    {
+        public int Max(List<int> list)
+        {
+            if (list.Count == 0)
+            {
+                throw new ArgumentException("The list can not be empty");
+            }
+
+            int max = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i] > max)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ MainSolution/UsingGenericRestrictions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UsingGenericRestrictions.Entities;
+
+namespace UsingGenericRestrictions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            Console.WriteLine("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            int max = calculationService.Max(list);
+
+            Console.WriteLine("Max:");
+            Console.WriteLine(max);
+
+        }
+    }
+}
