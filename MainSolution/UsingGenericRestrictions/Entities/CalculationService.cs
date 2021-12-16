47f5e017afa014121506c40d2f60000cd1a29a86
gabwestside <gabrielnfl13@gmail.com>
2021-12-16 14:28:40 -0300

    Att 1.0.28

--- MainSolution/UsingGenericRestrictions/Entities/CalculationService.cs
+++ MainSolution/UsingGenericRestrictions/Entities/CalculationService.cs
@@ -6,17 +6,17 @@ namespace UsingGenericRestrictions.Entities
 {
     class CalculationService
     {
-        public int Max(List<int> list)
+        public T Max<T>(List<T> list) where T : IComparable
         {
             if (list.Count == 0)
             {
                 throw new ArgumentException("The list can not be empty");
             }
 
-            int max = list[0];
+            T max = list[0];
             for (int i = 1; i < list.Count; i++)
             {
-                if (list[i] > max)
+                if (list[i].CompareTo(max) > 0)
                 {
                     max = list[i];
                 }
