9efae6d710f88d88ec37d66245c04ae588fe23e4
gabwestside <gabrielnfl13@gmail.com>
2021-12-17 15:34:28 -0300

    Att 1.0.32

---
+++ MainSolution/ProposedExercise02/Entities/LogRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProposedExercise02.Entities
+{
+    class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
---
+++ MainSolution/ProposedExercise02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using ProposedExercise02.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = name, Instant = instant });
+                        Console.WriteLine(line);
+                    }
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
