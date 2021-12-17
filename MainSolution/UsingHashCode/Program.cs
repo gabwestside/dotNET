2b11a5498941fcd726dae963df18d1a4cb4508de
gabwestside <gabrielnfl13@gmail.com>
2021-12-17 14:31:20 -0300

    Att 1.0.30

---
+++ MainSolution/UsingHashCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UsingHashCode.Entities;
+
+namespace UsingHashCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Gabs", Email = "gabs@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
