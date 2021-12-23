1b092524b9802ad4ea835108ba852464d6ff3041
gabwestside <gabrielnfl13@gmail.com>
2021-12-23 11:48:29 -0300

    Att 1.0.38

---
+++ MainSolution/UsingMulticastDelegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UsingMulticastDelegates.Services;
+
+namespace UsingMulticastDelegates
+{
+    class Program
+    {
+        delegate void BinaryNymericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNymericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+        }
+    }
+}
---
+++ MainSolution/UsingMulticastDelegates/Services/CalculationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingMulticastDelegates.Services
+{
+    class CalculationService
+    {
+        public static void ShowMax(double x, double y)
+        {
+            double max = (x > y) ? x : y;
+        }
+
+        public static void ShowSum(double x, double y)
+        {
+            double sum = x + y;
+            Console.WriteLine(sum);
+        }
+    }
+}
