26d8c904eb7d503ae40dde0ad805e25b689fe69c
gabwestside <gabrielnfl13@gmail.com>
2021-12-23 16:16:57 -0300

    Att 1.0.39

---
+++ MainSolution/UsingAction/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿
+namespace UsingAction.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", "
+                + Price.ToString("F2");
+        }
+    }
+}
---
+++ MainSolution/UsingAction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UsingAction.Entities;
+using System.Collections.Generic;
+
+namespace UsingAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.ForEach(p => { p.Price += p.Price * 0.1; });
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
