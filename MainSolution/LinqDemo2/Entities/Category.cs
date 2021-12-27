332fe08522f3c1d5ef77f267affc9aa0f0583bbb
gabwestside <gabrielnfl13@gmail.com>
2021-12-27 14:01:10 -0300

    Final att of chapter

---
+++ MainSolution/LinqDemo2/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿
+namespace LinqDemo2.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
---
+++ MainSolution/SolvedExercise2/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace SolvedExercise2.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
