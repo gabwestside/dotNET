71e4ff2dd8fdec094cd53704ceb73feefffa5b5c
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-02-13 13:22:29 -0300

    Adding new folder contents a RPG Games

---
+++ Game-RPG-main/Program.cs
@@ -0,0 +1,26 @@
+﻿using RPG.src.Entities;
+using System;
+
+namespace RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Knight arus = new Knight("Arus", 23, "Knight");
+            //Wizard rafael = new Wizard("Rafael", 23, "Wizard");
+            //Druid eduardo = new Druid("Eduardo", 23, "Druid");
+            //Swordsman Mangabeira = new Swordsman("Mangabeira", 23, "Swordsman");
+            //Hero glauber = new Hero("Glauber", 23, "Devil");
+
+            //Console.WriteLine(rafael.Attack(7));
+            //Console.WriteLine(rafael.Attack(1));
+            //Console.WriteLine(arus.Attack(1));
+            //Console.WriteLine(eduardo.Attack(1));
+            //Console.WriteLine(eduardo.Attack(7));
+            //Console.WriteLine(Mangabeira.Attack(1));
+            //Console.WriteLine(glauber);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Game-RPG-main/src/Entities/Swordsman.cs
@@ -0,0 +1,29 @@
+﻿namespace RPG.src.Entities
+{
+    public class Swordsman : Hero
+    {
+        public Swordsman(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+        public override string Attack()
+        {
+            return this.Name + " Atacou com a lança! ";
+        }
+
+        public string Attack(int Bonus)
+        {
+            if (Bonus > 6)
+            {
+                return this.Name + " Atacou com o galope do cavalo super efetiva + bônus de " + Bonus;
+            }
+            else
+            {
+                return this.Name + " Atacou com a lança + bônus fraco de " + Bonus;
+            }
+        }
+    }
+}
