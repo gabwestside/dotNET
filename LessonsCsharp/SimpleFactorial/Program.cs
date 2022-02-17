84540a0504b6e0c9abf79337817501ed01f16ae4
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-02-16 22:45:03 -0300

    Adding new project to do it a simple factorial formula

---
+++ LessonsCsharp/SimpleFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SimpleFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, factorial, count;
+
+            count = 0;
+            factorial = 1;
+
+            number = int.Parse(Console.ReadLine());
+
+            for (count = number; count >= 1; count--)
+            {
+                factorial = factorial * count;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
