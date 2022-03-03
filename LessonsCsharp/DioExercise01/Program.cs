c279ff606339d49cd731b70909102a079259f304
Gabriel Moura <gabriel.moura@fitbank.com.br>
2022-03-03 11:22:33 -0300

    Solving some challenges of the DIO

---
+++ LessonsCsharp/DioExercise01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace DioExercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input;
+
+            input = Console.ReadLine().Split(' ');
+            int cod1 = int.Parse(input[0]);
+            int n1 = int.Parse(input[1]);
+            double valor1 = double.Parse(input[2], CultureInfo.InvariantCulture);
+
+            input = Console.ReadLine().Split(' ');
+            int cod2 = int.Parse(input[0]);
+            int n2 = int.Parse(input[1]);
+            double valor2 = double.Parse(input[2], CultureInfo.InvariantCulture);
+
+            double total = (n1 * valor1) + (n2 * valor2);
+            Console.WriteLine("VALOR A PAGAR: R$ {0:F2}", total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ LessonsCsharp/DioExercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace DioExercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C, AVG;
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            AVG = (A * 0.2) + (B * 0.3) + (C * 0.5);
+
+            Console.WriteLine("MEDIA = {0:F1}", AVG.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
