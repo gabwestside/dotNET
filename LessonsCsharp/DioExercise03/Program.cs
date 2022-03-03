033773eb48831a910c2c66401c3a1a6acb8d6702
Gabriel Moura <gabrielnfl13@gmail.com>
2022-03-03 15:55:14 -0300

    Dio challenge - Santas exercise

--- LessonsCsharp/DioExercise03/Program.cs
+++ LessonsCsharp/DioExercise03/Program.cs
@@ -6,7 +6,13 @@ namespace DioExercise03
     {
         static void Main(string[] args)
         {
-           
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = i * n;
+                Console.WriteLine(string.Format($"{i} x {n} = {result}"));
+            }
         }
     }
 }
---
+++ LessonsCsharp/DioExercise05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DioExercise05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                if (i != N - 1)
+                {
+                    Console.Write("Ho ");
+                }
+                else
+                {
+                    Console.WriteLine("Ho!");
+                }
+            }
+        }
+    }
+}
