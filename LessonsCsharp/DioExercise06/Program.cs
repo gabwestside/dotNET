ab18d2b004610dfd27faaeb606b0c40eb87ce4f1
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-03-03 22:42:35 -0300

    PUM problem

---
+++ LessonsCsharp/DioExercise06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DioExercise06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int primeiro = 1;
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine(primeiro + " " + (primeiro + 1) + " " + (primeiro + 2) + " PUM ");
+                primeiro += 4;
+            }
+        }
+    }
+}
