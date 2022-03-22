b7bbd0ee7e3b2bffebf30553d6a4901e3851f5f3
Gabriel Moura <gabrielnfl13@gmail.com>
2022-03-22 16:35:52 -0300

    Finishing DIO chalenges

---
+++ LessonsCsharp/DioExercise08/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DioExercise08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] v = Console.ReadLine().Split();
+
+            //Insira o seu código aqui
+            int N = int.Parse(v[0]);
+            int L = int.Parse(v[1]);
+
+            int P = N * L;
+
+            Console.WriteLine(P);
+        }
+    }
+}
---
+++ LessonsCsharp/DioExercise09/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DioExercise09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int inicio = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(inicio + " " + (inicio * inicio) + " " + (inicio * inicio * inicio));
+                inicio++;
+            }
+        }
+    }
+}
