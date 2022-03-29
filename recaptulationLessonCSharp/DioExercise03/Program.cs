3781dba5342a646e68f6661c30b7b5508c814fc5
Gabriel Moura <gabrielnfl13@gmail.com>
2022-03-29 14:32:18 -0300

    Adding more dio chalenge

---
+++ recaptulationLessonCSharp/DioExercise03/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DioExercise03
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 3600, minute = 60;
+            int timeInSeconds = int.Parse(Console.ReadLine());
+
+            //TODO: Implementar a formula para calcular as horas.
+            int hours = timeInSeconds / hour;
+            timeInSeconds = timeInSeconds % (hour);
+
+            //TODO: Implementar a formula para calcular as horas.
+            int minutes = timeInSeconds / minute;
+            int seconds = timeInSeconds % minute;
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }
+}
