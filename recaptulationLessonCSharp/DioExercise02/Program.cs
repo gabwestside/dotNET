c4099cca5a05962c42a55464e19f9816bc71b093
Gabriel Moura <gabrielnfl13@gmail.com>
2022-03-29 14:16:04 -0300

    Adding a new exercise of DIO chalenges

---
+++ recaptulationLessonCSharp/DioExercise02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DioExercise02
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano = 365, mes = 30;
+
+            int dias = int.Parse(Console.ReadLine());
+            int anos = dias / ano;
+
+            dias = dias % ano;
+
+            int meses = dias / mes;
+            
+            dias = dias % mes;
+            
+            Console.WriteLine($"{anos} ano(s)");
+            Console.WriteLine($"{meses} mes(es)");
+            Console.WriteLine($"{dias} dia(s)");
+        }
+    }
+}
