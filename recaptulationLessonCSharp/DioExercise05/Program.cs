1716b0688ee30df3c61bbcd2a4f433cad230577e
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-03-29 20:14:15 -0300

    Senha fixa

---
+++ recaptulationLessonCSharp/DioExercise05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DioExercise05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string checkPwd = "2002";
+
+            string pwd = Console.ReadLine();
+
+            while (pwd != checkPwd)
+            {
+                Console.WriteLine("Senha Invalida");
+
+                pwd = Console.ReadLine();
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
