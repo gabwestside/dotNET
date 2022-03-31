0a174ed3275d8da60d697b2bb2d59d400915df3b
Gabriel Moura <gabrielnfl13@gmail.com>
2022-03-31 14:56:36 -0300

    Demo Value Types

---
+++ recaptulationLessonCSharp/TypesAndReferences/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+public class Program
+{
+    static int Adicionar20(int a)
+    {
+        return a + 20;
+    }
+
+    public static void Main()
+    {
+        int a = 2;
+        a = Adicionar20(a);
+
+        WriteLine($"O valor da variavel a é {a}");
+    }
+}
