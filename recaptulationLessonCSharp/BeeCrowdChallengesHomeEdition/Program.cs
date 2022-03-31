14879c116e16613e8c61551019de8c3d157cf5b0
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-03-30 22:20:22 -0300

    Many updates

---
+++ recaptulationLessonCSharp/BeeCrowdChallengesHomeEdition/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace BeeCrowdChallengesHomeEdition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // double radius, raidusPowered, pi = 3.14159;
+
+            // radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // raidusPowered = ((4.0/3) * pi * Math.Pow(radius, 3));
+
+            // Console.WriteLine($"VOLUME = {raidusPowered.ToString("F3", CultureInfo.InvariantCulture)}");
+
+            
+        }
+    }
+}
