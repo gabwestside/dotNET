92dd261873cf9a0028705fd7b2a60aeece1831f6
Gabriel Moura <gabrielnfl13@gmail.com>
2022-04-01 10:21:44 -0300

    Demo1 types and references

---
+++ recaptulationLessonCSharp/TypesAndReferences/Pessoa.cs
@@ -0,0 +1,9 @@
+namespace TypesAndReferences
+{
+    public class Pessoa
+    {
+        public int Idade { get; set; }
+        public string Nome{ get; set; }
+        public string Documento { get; set; }
+    }
+}
--- recaptulationLessonCSharp/TypesAndReferences/Program.cs
+++ recaptulationLessonCSharp/TypesAndReferences/Program.cs
@@ -1,17 +1,34 @@
-﻿using static System.Console;
+﻿using TypesAndReferences;
+using static System.Console;
 
 public class Program
 {
-    static int Adicionar20(int a)
-    {
-        return a + 20;
-    }
-
-    public static void Main()
+    static void Demo1()
     {
         int a = 2;
         a = Adicionar20(a);
 
         WriteLine($"O valor da variavel a é {a}");
+    }
+    static int Adicionar20(int a)
+    {
+        return a + 20;
+    }
+
+    static void TrocarNome(Pessoa p1, string novoNome) 
+    {
+        p1.Nome = novoNome;
+    }
+
+    public static void Main()
+    {
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Gabriel";
+        p1.Idade = 24;
+        p1.Documento = "1234";
+
+        TrocarNome(p1, "Wstside");
+
+        WriteLine($"O novo nome é: {p1.Nome}");
     }
 }
