aad27ecfc183e4a28df9fdbaa787f960365f4c37
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-07-27 22:35:34 -0300

    Studying serializing

---
+++ LessonsCsharp/SerializeXML/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Library;
+using System.Xml.Serialization;
+
+namespace SerializeXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User() { Name = "Joseph Costas", TaxNumber = "111.222.333.44", Mail = "joseph@mail.com"};
+
+            XmlSerializer serializer = new XmlSerializer(typeof(User));
+
+            StreamWriter streamWriter = new StreamWriter(@"C:\Users\Gabriel\Documents\temp\files");
+
+            serializer.Serialize(streamWriter, user);
+        }
+    }
+}
