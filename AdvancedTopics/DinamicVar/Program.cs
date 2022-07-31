0dc3830535973f819ed6c9f735cf18a9a24eb9d7
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-07-31 01:30:36 -0300

    Learning about object, var and dinamic types

---
+++ AdvancedTopics/DinamicVar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DinamicVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = "";
+            object t = 123;
+            t = "";
+
+            var v1 = "";
+            var v2 = 123;
+            // var v3;
+            // v2 = "";
+
+            dynamic d1 = new User() { Name = "Gab" };
+
+            Console.WriteLine(d1.Name);
+            // Console.WriteLine(d1.Mail);
+
+            d1 = "";
+
+            Console.ReadKey();
+        }
+
+        class User
+        {
+            public string Name { get; set; }
+        }
+    }
+}
