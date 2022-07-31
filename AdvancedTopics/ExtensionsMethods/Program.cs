acb4b8bd1ee550d1d9a4a6b699aa357b6e2b6885
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-07-31 01:46:06 -0300

    Learning to build a extension method

---
+++ AdvancedTopics/ExtensionsMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExtensionsMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "hello World!";
+            Console.WriteLine(value.FirstToUpper());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdvancedTopics/ExtensionsMethods/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExtensionsMethods
+{
+    public static class StringExtension
+    {
+        public static string FirstToUpper(this String str)
+        {
+            string firstLetter = str.Substring(0, 1);
+
+            string secondLetter = str.Substring(1);
+
+            return firstLetter.ToUpper() + secondLetter;
+        }
+    }
+}
