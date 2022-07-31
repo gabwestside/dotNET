049b1032bfb45090d5d84834b10f263437b34295
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-07-31 15:43:12 -0300

    Now im learning to work with LINQ

---
+++ AdvancedTopics/LINQ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 3, 10, 1, 4, 25, 2, 5, 12, 22, 11 };
+
+            var querylist = list.Where(x => x > 10)
+                                .OrderBy(x => x)
+                                .Select(x => x);
+
+            foreach (var item in querylist)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdvancedTopics/NewWayLINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace NewWayLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 3, 10, 1, 4, 25, 2, 5, 12, 22, 11 };
+
+            var querylist = from itens in list 
+                            where itens > 10 
+                            orderby itens 
+                            select itens;
+            int count = 1;
+
+            foreach (var item in querylist)
+            {
+                Console.WriteLine($"#{count} item: " + item);
+
+                count++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
