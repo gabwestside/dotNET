56cc2cc29ebb42eff687654a9366caa8523a959f
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-08-04 22:51:09 -0300

    Starting to work with Delegates

---
+++ AdvancedTopics/Delegates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        delegate int Calculate(int a, int b);
+
+        static void Main(string[] args)
+        {
+            Calculate calculate = new Calculate(Sum);
+            int sum = calculate(10, 20);
+
+            Calculate calculate1 = new Calculate(Subtract);
+            int sub = calculate1(25, 5);
+            /*
+                var sum = Sum(10, 20);
+            */
+
+            /*
+                var sub = Subtract(20, 10);
+            */
+
+            Console.WriteLine($"Sum = {sum}\nSub = {sub}");
+
+            Console.ReadKey();
+        }
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
