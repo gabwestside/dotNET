5bbc047d39c23afe21e9b177bb71373e7625e238
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-08-16 23:26:30 -0300

    creating delegate method

---
+++ AdvancedTopicsVol2/DelegateeMethod/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DelegateeMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnClick.Click += delegate (object sender, EventArgs e)
+            {
+                MessageBox.Show("Hi, I was clicked!");
+            };
+        }
+
+        //private void btnClick_Click(object sender, EventArgs e)
+        //{
+
+        //}
+    }
+}
